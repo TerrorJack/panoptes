3cc9b5a55283739acbaa56a4b420dc5b5260fded
Cheng Shao <terrorjack@type.dance>
2023-07-12 01:01:14 +0000

    Support rsp files and it works now

--- panoptes.cpp
+++ panoptes.cpp
@@ -4,13 +4,19 @@
 #include <sys/file.h>
 #include <unistd.h>
 
+#include <cctype>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-static void log_json_str(FILE *fp, const char *str) {
+static void log_json_str(FILE *fp, const std::string &str) {
   fputc('"', fp);
-  for (; *str != '\0'; ++str) {
-    switch (*str) {
+  for (const char c : str) {
+    switch (c) {
     case '\"': {
       fputs("\\\"", fp);
       break;
@@ -19,8 +25,20 @@ static void log_json_str(FILE *fp, const char *str) {
       fputs("\\\\", fp);
       break;
     }
+    case '\n': {
+      fputs("\\n", fp);
+      break;
+    }
+    case '\r': {
+      fputs("\\r", fp);
+      break;
+    }
+    case '\t': {
+      fputs("\\t", fp);
+      break;
+    }
     default: {
-      fputc(*str, fp);
+      fputc(c, fp);
       break;
     }
     }
@@ -28,17 +46,96 @@ static void log_json_str(FILE *fp, const char *str) {
   fputc('"', fp);
 }
 
+static void log_json_strs(FILE *fp, const std::vector<std::string> &strs) {
+  if (strs.empty()) {
+    fputs("[]", fp);
+    return;
+  }
+
+  fputc('[', fp);
+  log_json_str(fp, strs[0]);
+  for (int i = 1; i < strs.size(); ++i) {
+    fputc(',', fp);
+    log_json_str(fp, strs[i]);
+  }
+  fputc(']', fp);
+}
+
+static void lex_rsp(std::vector<std::string> &result, const std::string &rsp) {
+  std::string token;
+  bool in_quotes = false;
+
+  for (int i = 0;;) {
+    // Ensure at least 1 char left
+    if (i >= rsp.size()) {
+      break;
+    }
+
+    // Change lexer state, no new char
+    if (rsp[i] == '"') {
+      if (in_quotes) {
+        result.push_back(token);
+        token.clear();
+      }
+      in_quotes = !in_quotes;
+      ++i;
+      continue;
+    }
+
+    // Assume \" can only occur within quotes
+    if (i + 1 < rsp.size() && rsp[i] == '\\' && rsp[i + 1] == '"') {
+      token.push_back('"');
+      i += 2;
+      continue;
+    }
+
+    // Whitespace not in quotes is delimiter
+    if (isspace(rsp[i]) && !in_quotes) {
+      if (!token.empty()) {
+        result.push_back(token);
+        token.clear();
+      }
+      ++i;
+      continue;
+    }
+
+    // Regular char
+    token.push_back(rsp[i]);
+    ++i;
+  }
+
+  // Handle last token
+  if (!token.empty()) {
+    result.push_back(token);
+  }
+}
+
+static void expand_argv(std::vector<std::string> &result,
+                        const std::string &argv) {
+  if (!argv.empty() && argv[0] == '@') {
+    std::ifstream f(argv.substr(1));
+    std::stringstream buf;
+    buf << f.rdbuf();
+    lex_rsp(result, buf.str());
+    return;
+  }
+  result.push_back(argv);
+}
+
 static void log_exec_record(FILE *fp, const char *file, char *const argv[],
                             const char *cwd) {
   fputc('{', fp);
   log_json_str(fp, "arguments");
-  fputs(":[", fp);
-  log_json_str(fp, file);
+  fputc(':', fp);
+
+  std::vector<std::string> args{file};
   for (int i = 1; argv[i] != nullptr; ++i) {
-    fputc(',', fp);
-    log_json_str(fp, argv[i]);
+    expand_argv(args, argv[i]);
   }
-  fputs("],", fp);
+
+  log_json_strs(fp, args);
+
+  fputc(',', fp);
   log_json_str(fp, "directory");
   fputc(':', fp);
   log_json_str(fp, cwd);
@@ -58,7 +155,7 @@ static void log_exec(const char *file, char *const argv[]) {
 extern "C" int execvp(const char *file, char *const argv[]) {
   log_exec(file, argv);
   using ty_execvp = int (*)(const char *file, char *const argv[]);
-  const auto orig_execvp =
+  const static auto orig_execvp =
       reinterpret_cast<ty_execvp>(dlsym(RTLD_NEXT, "execvp"));
   return orig_execvp(file, argv);
 }
@@ -66,7 +163,8 @@ extern "C" int execvp(const char *file, char *const argv[]) {
 extern "C" int execv(const char *path, char *const argv[]) {
   log_exec(path, argv);
   using ty_execv = int (*)(const char *path, char *const argv[]);
-  const auto orig_execv = reinterpret_cast<ty_execv>(dlsym(RTLD_NEXT, "execv"));
+  const static auto orig_execv =
+      reinterpret_cast<ty_execv>(dlsym(RTLD_NEXT, "execv"));
   return orig_execv(path, argv);
 }
 
@@ -75,7 +173,7 @@ extern "C" int execvpe(const char *file, char *const argv[],
   log_exec(file, argv);
   using ty_execvpe =
       int (*)(const char *file, char *const argv[], char *const envp[]);
-  const auto orig_execvpe =
+  const static auto orig_execvpe =
       reinterpret_cast<ty_execvpe>(dlsym(RTLD_NEXT, "execvpe"));
   return orig_execvpe(file, argv, envp);
 }
@@ -85,7 +183,7 @@ extern "C" int execve(const char *filename, char *const argv[],
   log_exec(filename, argv);
   using ty_execve =
       int (*)(const char *filename, char *const argv[], char *const envp[]);
-  const auto orig_execve =
+  const static auto orig_execve =
       reinterpret_cast<ty_execve>(dlsym(RTLD_NEXT, "execve"));
   return orig_execve(filename, argv, envp);
 }
@@ -99,7 +197,7 @@ extern "C" int posix_spawn(pid_t *pid, const char *path,
                                  const posix_spawn_file_actions_t *file_actions,
                                  const posix_spawnattr_t *attrp,
                                  char *const argv[], char *const envp[]);
-  const auto orig_posix_spawn =
+  const static auto orig_posix_spawn =
       reinterpret_cast<ty_posix_spawn>(dlsym(RTLD_NEXT, "posix_spawn"));
   return orig_posix_spawn(pid, path, file_actions, attrp, argv, envp);
 }
@@ -113,7 +211,7 @@ extern "C" int posix_spawnp(pid_t *pid, const char *file,
       pid_t *pid, const char *file,
       const posix_spawn_file_actions_t *file_actions,
       const posix_spawnattr_t *attrp, char *const argv[], char *const envp[]);
-  const auto orig_posix_spawnp =
+  const static auto orig_posix_spawnp =
       reinterpret_cast<ty_posix_spawnp>(dlsym(RTLD_NEXT, "posix_spawnp"));
   return orig_posix_spawnp(pid, file, file_actions, attrp, argv, envp);
 }
